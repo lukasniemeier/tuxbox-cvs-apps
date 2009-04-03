e03cf0472f1dc8fbd19306d6c16e494d259ae856
seife <seife>
2009-04-03 15:30:22 +0000

    zapit: fix LNB passthrough on dream
    This fixes the LNB passthrough if zapit is put into standby state.
    Additionally, it saves much more power (on dm500s, power consumption
    now goes down from 13.7 to 7.3 Watts if zapit is put into standby).

--- dvb/zapit/src/zapost/frontend.cpp
+++ dvb/zapit/src/zapost/frontend.cpp
@@ -1,5 +1,5 @@
 /*
- * $Id: frontend.cpp,v 1.65 2009/03/22 22:06:26 rhabarber1848 Exp $
+ * $Id: frontend.cpp,v 1.66 2009/04/03 15:30:22 seife Exp $
  *
  * (C) 2002-2003 Andreas Oberritter <obi@tuxbox.org>
  *
@@ -89,11 +89,19 @@ CFrontend::~CFrontend(void)
 	if (diseqcType > MINI_DISEQC)
 		sendDiseqcStandby();
 
-	close(fd);
+	/* tested on dm500, VOLTAGE_OFF switched into passthrough mode,
+	   FE_POWER_OFF does something else to save some power...
+	   It does no harm on dbox2, at least not on philips sat
+	   enigma does exactly the same it its savePower() function
+	 */
+	secSetVoltage(SEC_VOLTAGE_OFF, 1);
+	secSetTone(SEC_TONE_OFF, 1);
 #if HAVE_DVB_API_VERSION < 3
+	fop(ioctl, FE_SET_POWER_STATE, FE_POWER_OFF);
 	if (secfd >= 0)
 		close(secfd);
 #endif
+	close(fd);
 }
 
 void CFrontend::reset(void)
