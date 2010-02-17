2174f525cc88c2300b338868b25e082a5db14668
seife <seife>
2010-02-17 10:18:30 +0000

    movieplayer2: fix file time overflow problem

--- tuxbox/neutrino/src/gui/movieplayer2.cpp
+++ tuxbox/neutrino/src/gui/movieplayer2.cpp
@@ -3454,7 +3454,7 @@ static void checkAspectRatio (int /*vdec*/, bool /*init*/)
 std::string CMoviePlayerGui::getMoviePlayerVersion(void)
 {
 	static CImageInfo imageinfo;
-	return imageinfo.getModulVersion("Movieplayer2 ","$Revision: 1.66 $");
+	return imageinfo.getModulVersion("Movieplayer2 ","$Revision: 1.67 $");
 }
 
 void CMoviePlayerGui::showFileInfoVLC()
@@ -3526,12 +3526,18 @@ static inline int get_filetime(bool remaining)
 {
 	int filetime;
 	if (remaining)
+	{
 		filetime = g_endpts - g_pts;
+		if (g_pts < g_startpts)
+			filetime -= (int)(0x1ffffffffLL / 90);
+	}
 	else
+	{
 		filetime = g_pts - g_startpts;
+		if (filetime < 0)
+			filetime += (int)(0x1ffffffffLL / 90);
+	}
 
-	if (filetime < 0)
-		filetime += (int)(0x1ffffffffLL / 90);
 	filetime /= 1000;
 	return filetime;
 }
