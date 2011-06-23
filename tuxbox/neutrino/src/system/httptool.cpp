d31be4be9cd69b26d83b3744a239d45dc88b6cc3
rhabarber1848 <rhabarber1848>
2011-06-23 18:51:27 +0000

    Neutrino: Follow HTML links during image remote-updates, patch by Gaucho316: http://www.dbox2-tuning.net/forum/viewtopic.php?p=379792#p379792

--- tuxbox/neutrino/src/system/httptool.cpp
+++ tuxbox/neutrino/src/system/httptool.cpp
@@ -88,6 +88,7 @@ bool CHTTPTool::downloadFile(const std::string & URL, const char * const downloa
 		curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0);
 		curl_easy_setopt(curl, CURLOPT_USERAGENT, userAgent.c_str());
 		curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1);
+		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION,1);
 		//curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
 
 		if(strcmp(g_settings.softupdate_proxyserver,"")!=0)
