78fa4f888479b06ee927b9d35f0f5c732438446f
svenhoefer <svenhoefer@svenhoefer.com>
2013-12-21 21:12:25 +0100

    Neutrino CMenuWidget: don't add a separatorline ...
    ... when no button and no section text is given
    
    Signed-off-by: Christian Schuett <Gaucho316@hotmail.com>
    Signed-off-by: Thilo Graf <dbt@novatux.de>

--- tuxbox/neutrino/src/gui/widget/menue.cpp
+++ tuxbox/neutrino/src/gui/widget/menue.cpp
@@ -709,6 +709,7 @@ void CMenuWidget::addIntroItems(neutrino_locale_t subhead_text, neutrino_locale_
 {
 	if (subhead_text != NONEXISTANT_LOCALE)
 		addItem(new CMenuSeparator(CMenuSeparator::ALIGN_LEFT | CMenuSeparator::SUB_HEAD | CMenuSeparator::STRING, subhead_text));
+
 	addItem(GenericMenuSeparator);
 
 	if (buttontype != BTN_TYPE_NO)
@@ -726,7 +727,7 @@ void CMenuWidget::addIntroItems(neutrino_locale_t subhead_text, neutrino_locale_
 
 	if (section_text != NONEXISTANT_LOCALE)
 		addItem(new CMenuSeparator(CMenuSeparator::LINE | CMenuSeparator::STRING, section_text));
-	else
+	else if (buttontype != BTN_TYPE_NO)
 		addItem(GenericMenuSeparatorLine);
 }
 
